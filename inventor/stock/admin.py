f4d8b2bb8bfef7a9ccf19180b30f449b0e3be4d6
Horacio Duran <hduran@machinalis.com>
2012-05-22 13:00:03 -0300

    Tuned the model, added some useful info in the admin

--- inventor/stock/admin.py
+++ inventor/stock/admin.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from stock.model import Brand, PartType, PartState, PhysicalLocation, Station, Part
+from stock.models import Brand, PartType, PartState, PhysicalLocation, Station, Part
 
 class BrandAdmin(admin.ModelAdmin):
    list_display = ('name',) 
 
 admin.site.register(Brand, BrandAdmin)
 
-class PartTypeAdmin(admin.ModelAdmin)
+class PartTypeAdmin(admin.ModelAdmin):
     list_display = ('name',) 
 
 admin.site.register(PartType, PartTypeAdmin)
 
 class PartStateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_usable')
-    filter_horizontal = ('is_usable',)
+    list_display = ('state_name', 'state_is_usable')
+    list_filter = ('state_is_usable',)
 
-admin.site.register(PartType, PartTypeAdmin)
+admin.site.register(PartState, PartStateAdmin)
 
 class PhysicalLocationAdmin(admin.ModelAdmin):
     list_display = ('name',) 
 
-admin.site.register(PartType, PartTypeAdmin)
+admin.site.register(PhysicalLocation, PhysicalLocationAdmin)
 
 class PartAdmin(admin.ModelAdmin):
-    list_display = ('part_type', 'part_brand', 'part_spec', 'part_state', 'part_location')
+    list_display = ('part_brand', 'part_spec', 'part_state', 'part_location')
     search_fields = ('part_type', 'part_brand', 'part_spec', 'part_location')
-    filter_horizontal = ('part_type', 'part_brand', 'part_location', 'part_state')
+    list_filter = ('part_type', 'part_brand', 'part_location', 'part_state')
 
 class InlinePartAdmin(admin.TabularInline):
     model = Part
 
-admin.site.register(PartType, PartTypeAdmin)
+admin.site.register(Part, PartAdmin)
 
 class StationAdmin(admin.ModelAdmin):
     list_display = ('station_name', 'physical_location', 'location_owner')
-    filter_horizontal = ('physical_location', ) 
+    list_filter = ('physical_location', ) 
     inlines = (InlinePartAdmin, )
 
-admin.site.register(PartType, PartTypeAdmin)
+admin.site.register(Station, StationAdmin)
 
 
--- inventor/stock/models.py
+++ inventor/stock/models.py
@@ -4,29 +4,54 @@ from django.db import models
 class Brand(models.Model):
     name = models.CharField(u"Brand Name", max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 class PartType(models.Model):
     name = models.CharField(u"Part Type", max_length=100, help_text=u"Such as Hard Drive or Printer")
+
+    def __unicode__(self):
+        return self.name
 
 class PartState(models.Model):
     state_name = models.CharField(u"State denomination", max_length=100, help_text=u"such as broken or in repair")
     state_is_usable = models.BooleanField(u"Can be used in this state?")
 
+    def __unicode__(self):
+        usable = self.state_is_usable and "usable" or "not usable"
+        return u"%s (%s)" % (self.state_name, usable)
+
 class PhysicalLocation(models.Model):
     name = models.CharField(u"Name of the place", max_length=100, help_text=u"What do we name this place")
-    description = models.CharField(u"Location Description", max_length=255, help_text=u"Where is this, for instance, the kitchen")
+    description = models.CharField(u"Location Description", max_length=255, help_text=u"Where is this, for instance, "
+    "the kitchen", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
 
 class Station(models.Model):
     station_name = models.CharField(u"Name the location", max_length=100, help_text=u"Typically a workstation, use the"
                                                                                     "hostname if any")
-    physical_location = models.ForeignKey(PhysicalLocation)
-    location_owner = models.CharField(u"Who is responsable for this", max_length=100, help_text=u"Just the name")
+    physical_location = models.ForeignKey(PhysicalLocation, blank=True, null=True)
+    location_owner = models.CharField(u"Who is responsable for this", max_length=100, help_text=u"Just the name",
+    blank=True, null=True)
+
+    def __unicode__(self):
+        return u"%s @ %s (%s)" % (self.station_name, self.physical_location, self.location_owner)
 
 class Part(models.Model):
-    part_type = models.ForeignKey(u"Part Type", PartType)
-    part_brand = models.ForeignKey(u"Brand", Brand)
-    part_spec = models.CharField(u"Specs", max_length=255, help_text=u"Specs can be more identifying traces, such as 1G,
-    Sata, etc. for Hard Drives")
-    reference_url = models.URLField(u"Reference URL", help_text=u"A URL with info regarding the part, manufacturer one
-    is prefered")
-    part_state = models.ForeignKey(u"State of this part", PartState)
-    part_location = models.ForeignKey(u"Where is this?", Station, help_text=u"Typically a workstation")
+    part_type = models.ForeignKey(PartType, verbose_name=u"Part Type")
+    part_brand = models.ForeignKey(Brand, verbose_name=u"Brand")
+    part_spec = models.CharField(u"Specs", max_length=255, help_text=u"Specs can be more identifying traces, such as 1G,"
+    "Sata, etc. for Hard Drives", blank=True, null=True)
+    reference_url = models.URLField(u"Reference URL", help_text=u"A URL with info regarding the part, manufacturer one"
+    " is prefered", blank=True, null=True)
+    part_state = models.ForeignKey(PartState, verbose_name=u"State of this part", blank=True, null=True)
+    part_location = models.ForeignKey(Station, verbose_name=u"Where is this?", help_text=u"Typically a workstation",
+    blank=True, null=True)
+    part_physical_identifier = models.CharField(u"Physical Identifier", max_length=100, help_text=u"The text on the"
+    "label dude", blank=True, null=True)
+
+    def __unicode__(self):
+        return u"%s %s %s @ %s (%s)" % (self.part_type, self.part_brand, self.part_spec, self.part_location,
+                                            self.part_state)
