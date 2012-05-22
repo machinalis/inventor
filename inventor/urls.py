1381570e5f1088ca53392594087f9a07ec5f5dc0
Horacio Duran <hduran@machinalis.com>
2012-05-22 13:03:26 -0300

    Added a redirect so we always land in admin

--- inventor/urls.py
+++ inventor/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/admin'}),
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
