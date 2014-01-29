e7f7615a8de360f8ddf1944982192dd8a04ae8f7
Julian Bigatti <jbigatti@gmail.com>
2014-01-29 09:15:02 -0300

    Is prepared to migration to django 1.6

--- inventor/stock/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from stock import views
-
-urlpatterns = patterns('',
-    url(r'uncontained_parts$', views.UncontainedPartsView.as_view(),
-        name='uncontained_parts'),
-)
